9442744fed67d09950e389f72c902fa296b3a267
javajungto <javajungto@gmail.com>
2023-03-17 15:53:32 +0900

    Update auto_yt_dlp.sh

--- auto_yt_dlp.sh
+++ auto_yt_dlp.sh
@@ -5,11 +5,15 @@ if [ $# -eq 0 ]; then
   exit 1
 fi
 
+echo "url: $1"
+
 # download video using yt-dlp
-downloadFile=$(yt-dlp "$1" | grep Merging | sed 's/.*"\(.*\.webm\)".*/\1/')
+downloadFile=$(yt-dlp $1 | grep Destination | sed 's/.*Destination: \(.*\)/\1/')
+
+echo "1. downloaded ... : ${downloadFile}"
 
 # convert webm to mp3 using ffmpeg
-ffmpeg -i "$downloadFile" -b:a 320K "${downloadFile%.webm}.mp3"
+ffmpeg -i "$downloadFile" -b:a 320K "${downloadFile}.mp3"
 
 # remove original webm file
-rm "$downloadFile"
+rm -rf "$downloadFile"
